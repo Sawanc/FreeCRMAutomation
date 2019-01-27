9fc0814e501bf6b963d03f140eda4e4254b69776
Sawanc <sa193choubisa@gmail.com>
2019-01-27 16:55:04 +0530

    Update LoginPage.java

--- src/main/java/com/crm/qa/pages/LoginPage.java
+++ src/main/java/com/crm/qa/pages/LoginPage.java
@@ -14,7 +14,7 @@ import com.crm.qa.utilities.TestUtil;
  public class LoginPage extends TestBase
 
 {
-    //Page Factory //Object Repository	
+    //Page Factory //Object Repository and Comment added 	
 	WebDriverWait expwait;
 	
 	@FindBy(name="username")
